0d3dc4d5da84757bd27763d0d6a01516bdbd83f4
Omkar Ghate <otg0231@gmail.com>
2021-08-17 12:41:08 +0530

    style: submit button

--- src/App.js
+++ src/App.js
@@ -69,7 +69,7 @@ export default function App() {
               required
             />
 
-            <button type="submit" className="btn">
+            <button type="submit" className="submitBtn">
               Check
             </button>
           </form>
